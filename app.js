6098cdf172f7d41bf924f180f187b99fc87e9f34
Sunil <sunilrk.one@gmail.com>
2021-04-04 22:34:42 +0530

    initial api call to weatherstack done

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-console.log("Starting")
-setTimeout(()=>{
-	console.log("2s timer")
-}, 2000)
-setTimeout(()=>{
-	console.log("0s timer")
-}, 0)
-console.log("Ending")
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=e18066b0036fd38f8007781c592bd92c&query=31.2232,75.7670&units=f'
+
+request({url: url, json: true}, (err, res)=>{
+	console.log(`${res.body.current.weather_descriptions[0]}. it is ${res.body.current.temperature} degree out there and feels like ${res.body.current.feelslike} degree`)
+})
